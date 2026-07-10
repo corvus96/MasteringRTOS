24504c60667300bed89406155bd06129460fe89f
Guillermo Raven <45345200+corvus96@users.noreply.github.com>
2026-07-09 23:49:40 -0400

    Add FreeRTOSConfig.h and FreeRTOS directly into the project

---
+++ RTOS_Workspace/FreeRTOS_Projects/Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/stack_macros.h
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/stack_macros.h:
