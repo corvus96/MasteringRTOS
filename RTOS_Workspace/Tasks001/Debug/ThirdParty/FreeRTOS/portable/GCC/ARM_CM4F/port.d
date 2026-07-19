ddce618dfda30019a6490ae7de4223218e5b5963
Guillermo Raven <45345200+corvus96@users.noreply.github.com>
2026-07-19 00:49:23 -0400

    Add Hello word FreeRTOS with 2 task
    2 task using pre-emptive scheduling which are able to print in console

---
+++ RTOS_Workspace/Tasks001/Debug/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,20 @@
+ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o: \
+ ../ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.c \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/list.h
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/guill/Documents/STM32Cube/MasteringRTOS/RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/include/list.h:
