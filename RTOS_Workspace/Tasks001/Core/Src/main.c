9548cd26993de8471670a78016ff1368531814bf
Guillermo Raven <45345200+corvus96@users.noreply.github.com>
2026-07-19 23:27:38 -0400

    Update main.c
    Enable Cycle count function and activate system view Recording before any FreeRTOS API call

--- RTOS_Workspace/Tasks001/Core/Src/main.c
+++ RTOS_Workspace/Tasks001/Core/Src/main.c
@@ -44,7 +44,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+#define DWT_CTRL 	(*(volatile uint32_t*) 0xE0001000)
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -93,6 +93,12 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+  // Enable The CYCCNT counter (Cycle count function is enable for SEGGER)
+  DWT_CTRL |= (1 << 0);
+  // To configure and Start systemview recording
+  SEGGER_SYSVIEW_Conf();
+  SEGGER_SYSVIEW_Start();
+
   status = xTaskCreate(task1_handler, "Task-1", 200, "Hello world from Task-1", 2, &task1_handle);
   configASSERT(status == pdPASS);
   status = xTaskCreate(task2_handler, "Task-2", 200, "Hello world from Task-2", 2, &task2_handle);
