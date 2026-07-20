d911ce856fa1b3c18b648086916c092d15f2e95f
Guillermo Raven <45345200+corvus96@users.noreply.github.com>
2026-07-19 21:44:25 -0400

    Update FreeRTOSConfig.h

--- RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/FreeRTOSConfig.h
+++ RTOS_Workspace/Tasks001/ThirdParty/FreeRTOS/FreeRTOSConfig.h
@@ -659,6 +659,7 @@
 #define INCLUDE_xTaskGetCurrentTaskHandle      1
 #define INCLUDE_uxTaskGetStackHighWaterMark    0
 #define INCLUDE_xTaskGetIdleTaskHandle         1
+#define INCLUDE_pxTaskGetStackStart			   1
 #define INCLUDE_eTaskGetState                  0
 #define INCLUDE_xTimerPendFunctionCall         0
 #define INCLUDE_xTaskAbortDelay                0
@@ -697,4 +698,5 @@ standard names. */
 #define xPortPendSVHandler PendSV_Handler
 #define xPortSysTickHandler SysTick_Handler
 
+#include "SEGGER_SYSVIEW_FreeRTOS.h"
 #endif /* FREERTOS_CONFIG_H */
